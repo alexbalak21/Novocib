aae6e6e37cfd38576440f7cc3a48ca14a74b1f66
alexbalak21 <alex.balak@outlook.com>
2026-02-17 15:52:32 +0100

    implemented maintenance message

--- app/views/home.php
+++ app/views/home.php
@@ -88,6 +88,25 @@ require_once $_SERVER['DOCUMENT_ROOT'] . "/app/templates/new_base.php";
 </div>
 
 <div class="container mt-4">
+
+    <!-- MAINTENANCE MESSAGE -->
+    <div class="alert alert-success border-success-subtle bg-success-subtle py-4 mt-3 border rounded alert-dismissible fade show" role="alert">
+        <h4 class="display-6 lead text-center">
+            <i class="fa-solid fa-circle-info"></i> Scheduled Maintenance
+        </h4>
+
+        <p class="text-center mt-4 mb-0">
+            We will be performing maintenance on our servers.
+            Our services and email systems will be temporarily unavailable for a few days. <br>
+            We will work to restore everything as quickly as possible.
+        </p>
+
+        <p class="text-center mt-3">Thank you for your patience.</p>
+
+        <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+    </div>
+    <!-- END MAINTENANCE MESSAGE -->
+
     <div class="d-flex justify-content-center">
         <div class="searchbar">
             <input id="searchInput" class="search_input" type="text" name="search" placeholder="Search...">
