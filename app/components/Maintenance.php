aa12d74be91586e3c70fdc9b5adf9951e34d081f
alexbalak21 <alex.balak@outlook.com>
2026-02-22 21:25:11 +0100

    removed Maintenance message

---
+++ app/components/Maintenance.php
@@ -0,0 +1,17 @@
+    <!-- MAINTENANCE MESSAGE -->
+    <div class="alert alert-success border-success-subtle bg-success-subtle py-4 mt-3 border rounded alert-dismissible fade show" role="alert">
+        <h4 class="display-6 lead text-center">
+            <i class="fa-solid fa-circle-info"></i> Scheduled Maintenance
+        </h4>
+
+        <p class="text-center mt-4 mb-0">
+            We will be performing maintenance on our servers.
+            Our services and email systems will be temporarily unavailable for a few days. <br>
+            We will work to restore everything as quickly as possible.
+        </p>
+
+        <p class="text-center mt-3">Thank you for your patience.</p>
+
+        <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+    </div>
+    <!-- END MAINTENANCE MESSAGE -->
